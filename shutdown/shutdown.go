b735f40c8ad403c9cd7b5b0512d8c9bf543fecb9
Hendra Tommy Wijaya <hendra.tommy.w@gmail.com>
2023-02-17 18:05:19 +0700

    add shutdown submodules

---
+++ shutdown/shutdown.go
@@ -0,0 +1,124 @@
+package shutdown
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/hexastack-dev/devkit-go/log"
+)
+
+type signalNotifier interface {
+	Notify(c chan<- os.Signal, sig ...os.Signal)
+}
+
+type osSignalNotifierFunc func(c chan<- os.Signal, sig ...os.Signal)
+
+func (n osSignalNotifierFunc) Notify(c chan<- os.Signal, sig ...os.Signal) {
+	n(c, sig...)
+}
+
+type Shutdown struct {
+	logger    log.Logger
+	timeout   time.Duration
+	listeners map[string]Listener
+	notifier  signalNotifier
+}
+
+type Listener interface {
+	OnShutdown(context.Context) error
+}
+
+type ListenerFunc func(context.Context) error
+
+func (f ListenerFunc) OnShutdown(ctx context.Context) error {
+	return f(ctx)
+}
+
+func New(logger log.Logger, timeout time.Duration, listeners map[string]Listener) *Shutdown {
+	return &Shutdown{
+		logger:    logger,
+		timeout:   timeout,
+		listeners: listeners,
+		notifier:  osSignalNotifierFunc(signal.Notify),
+	}
+}
+
+// Wait receive an message that will be printed out to logger before blocking.
+// Wait block the call and wait for SIGINT, SIGTERM or SIGHUP signals.
+// After signal is received, this method will run Listener.OnShutdown in parallel,
+// the operation must complete under specified timeout. If any listeners return an
+// error or operation is timed out this method will call os.Exit(1).
+func (s *Shutdown) Wait(msg string) (os.Signal, error) {
+	quit := make(chan os.Signal, 1)
+	defer close(quit)
+
+	s.notifier.Notify(quit, os.Interrupt, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	if msg != "" {
+		getLogger(s.logger).Info(msg)
+	}
+	// wait for shutdown signals
+	sig := <-quit
+	getLogger(s.logger).Info(fmt.Sprintf("Received signal %s, shutting down", sig.String()))
+	// timeoutFunc := time.AfterFunc(s.timeout, func() {
+	// 	err := fmt.Errorf("shutdown did not complete after %d%s", s.timeout.Milliseconds(), "ms")
+	// 	getLogger(s.logger).Error("Shutdown timeout", err)
+	// 	os.Exit(1)
+	// })
+	// defer timeoutFunc.Stop()
+
+	done := make(chan bool, 1)
+
+	ctx, cancel := context.WithTimeout(context.Background(), s.timeout)
+	defer cancel()
+
+	go func() {
+		done <- s.onShutdown(ctx)
+	}()
+
+	select {
+	case <-ctx.Done():
+		err := fmt.Errorf("shutdown: shutdown did not complete after %d%s: %w", s.timeout.Milliseconds(), "ms", ctx.Err())
+		return sig, err
+	case ok := <-done:
+		if !ok {
+			return sig, errors.New("shutdown: shutdown completed with error")
+		} else {
+			return sig, nil
+		}
+	}
+}
+
+func (s *Shutdown) onShutdown(ctx context.Context) bool {
+	var (
+		wg sync.WaitGroup
+		ok bool
+	)
+	for name, listener := range s.listeners {
+		wg.Add(1)
+		go func(name string, listener Listener) {
+			defer wg.Done()
+
+			getLogger(s.logger).Debug(fmt.Sprintf("Running shutdown listener: %s", name))
+			if err := listener.OnShutdown(ctx); err != nil {
+				getLogger(s.logger).Error(fmt.Sprintf("Shutdown listener %s return an error", name), err)
+			} else {
+				ok = true
+			}
+		}(name, listener)
+	}
+	wg.Wait()
+	return ok
+}
+
+func getLogger(logger log.Logger) log.Logger {
+	if logger == nil {
+		return log.GetLogger()
+	}
+	return logger
+}
---
+++ shutdown/shutdown_test.go
@@ -0,0 +1,101 @@
+package shutdown
+
+import (
+	"context"
+	"errors"
+	"os"
+	"sync"
+	"testing"
+	"time"
+)
+
+type signalConsumer struct {
+	c   chan<- os.Signal
+	sig []os.Signal
+}
+
+type signalNotifierTest struct {
+	consumers []*signalConsumer
+	mutex     sync.Mutex
+}
+
+func (s *signalNotifierTest) Notify(c chan<- os.Signal, sig ...os.Signal) {
+	s.mutex.Lock()
+	con := &signalConsumer{
+		c:   c,
+		sig: sig,
+	}
+	s.consumers = append(s.consumers, con)
+	s.mutex.Unlock()
+}
+
+func (s *signalNotifierTest) notify(sig os.Signal) {
+	for _, con := range s.consumers {
+		for _, sig0 := range con.sig {
+			if sig0 == sig {
+				con.c <- sig
+				break
+			}
+		}
+	}
+}
+
+func TestShutdown(t *testing.T) {
+	notifier := &signalNotifierTest{}
+	res := make(chan string, 1)
+	defer close(res)
+
+	var (
+		sleep = 10 * time.Millisecond
+		wg    sync.WaitGroup
+	)
+
+	wg.Add(1)
+	// positive test
+	go func() {
+		defer wg.Done()
+		listeners := make(map[string]Listener)
+		listeners["test"] = ListenerFunc(func(ctx context.Context) error {
+			res <- "ok"
+			return nil
+		})
+		sh := New(nil, 2*sleep, listeners)
+		sh.notifier = notifier
+		sig, err := sh.Wait("")
+		if err != nil {
+			t.Errorf("should not return any error: %v", err)
+		}
+		if sig != os.Interrupt {
+			t.Errorf("should received os.Interrupt: %v", sig.String())
+		}
+		if out := <-res; out != "ok" {
+			t.Errorf("result should equals ok: %s", out)
+		}
+	}()
+
+	wg.Add(1)
+	// negative test, should be timeout
+	go func() {
+		defer wg.Done()
+
+		listeners := make(map[string]Listener)
+		listeners["test"] = ListenerFunc(func(ctx context.Context) error {
+			time.Sleep(2 * sleep)
+			return nil
+		})
+
+		sh := New(nil, time.Millisecond, listeners)
+		sh.notifier = notifier
+		_, err := sh.Wait("")
+		if !errors.Is(err, context.DeadlineExceeded) {
+			t.Errorf("should return timeout error: context deadline exceeded: %v", err)
+		}
+	}()
+
+	go func() {
+		time.Sleep(sleep)
+		notifier.notify(os.Interrupt)
+	}()
+
+	wg.Wait()
+}
